38a7f808c6319dd451c25afee9c518abf053e855
Harman Pannu <iharmanpannu@gmail.com>
2018-04-06 11:52:30 -0700

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,43 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import Header from './Header';
 
 import ContestList from './ContestList';
 
 const pushState = (obj, url) => window.history.pushState(obj, '', url);
 class App extends Component {
-  state = { message: 'Naming Contests', contests: this.props.initialContests };
+  state = {
+    message: 'Naming Contests',
+    contests: this.props.initialContests
+  };
 
   fetchContest = contestId => {
-    pushState({ currentContestId: contestId }, `/contest/${contestId}`);
-    this.setState();
-    console.log()
-  }
+    pushState({
+      currentContestId: contestId
+    }, `/contest/${contestId}`);
+    this.setState()
+  };
+
 
   render() {
-    // debugger;
-    return (
-      <div className="App">
-        <Header message={this.state.message} />
-        <ContestList
-          onContestClick={this.fetchContest}
-          contests={this.state.contests}
-        />
-        <div />
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      Header message = {
+        this.state.message
+      }
+      /> <
+      ContestList onContestClick = {
+        this.fetchContest
+      }
+      contests = {
+        this.state.contests
+      }
+      /> <
+      div / >
+      <
+      /div>
     );
   }
 }
